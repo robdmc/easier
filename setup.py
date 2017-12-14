e3b4741a94ccde43947af6a66499df15d953aaae
Rob deCarvalho <robdmc@gmail.com>
2017-12-14 11:51:38 -0500

    wip

--- setup.py
+++ setup.py
@@ -51,8 +51,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
