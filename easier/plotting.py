ae4f18bf0d654b0c2fdeb27e9d087b8250f249dd
Rob deCarvalho <robdmc@gmail.com>
2018-07-27 10:48:23 -0400

    made figure return an axis object.

--- easier/plotting.py
+++ easier/plotting.py
@@ -1,10 +1,13 @@
-import pylab as pl
-
-
 def figure(*args, grid=True, style='default', figsize=(9, 5), **kwargs):
+    """
+    Returns a matplotlib axis object.
+    """
+    import pylab as pl
     available = [s for s in pl.style.available + ['default'] if not s.startswith('_')]
     if style not in available:
         raise ValueError(f'\n\n Valid Styles are {available}')
     pl.style.use(style)
     pl.figure(*args, figsize=figsize, **kwargs)
     pl.grid(grid)
+    ax = pl.gca()
+    return ax
