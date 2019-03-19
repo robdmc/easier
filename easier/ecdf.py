d18339ceca65a0f51c83572d7d189f3be2243ea9
Rob deCarvalho <robdmc@gmail.com>
2019-03-19 14:38:05 -0400

    added inverse parameter to ecdf

--- easier/ecdf.py
+++ easier/ecdf.py
@@ -1,9 +1,10 @@
-def ecdf(x, N=100):
+def ecdf(x, N=100, inverse=False):
     """
     Thin wrapper around statsmodels ecdf.
     Arguments:
         x: array of points for which to find ecdf
         N: The number of output points you want in your ecdf
+        inverse: Return 1 - ecdf
     Returns:
         x_out: an array of values
         y_out: an array of percentiles
@@ -12,4 +13,6 @@ def ecdf(x, N=100):
     from statsmodels.distributions.empirical_distribution import ECDF
     x_out = np.linspace(min(x), max(x), N)
     y_out = ECDF(x)(x_out)
+    if inverse:
+        y_out = 1 - y_out
     return x_out, y_out
