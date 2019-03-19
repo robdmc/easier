a6226d5232533f77cbd590f8158f2ac525fbf6d3
Rob deCarvalho <robdmc@gmail.com>
2019-03-19 14:12:21 -0400

    made colorcycle into a genuine class

--- easier/__init__.py
+++ easier/__init__.py
@@ -10,7 +10,7 @@ from .memory import mem_show, mem_get
 import easier.hvtools as hv
 from .hvtools import cc, hist
 from .print_catcher import PrintCatcher
-from .plotting import figure
+from .plotting import figure, get_cc
 from .fit import Fit
 from .crypt import Crypt
 from .ecdf import ecdf
@@ -29,11 +29,6 @@ DictChatty = ChattyDict
 
 # loads a holoviews color cycler as cc defaulting to None if not available
 try:
-    def get_cc():
-        from string import ascii_lowercase
-        import holoviews as hv
-        cc = type('color', (), dict(zip(ascii_lowercase, hv.Cycle().default_cycles['default_colors'])))
-        return cc
     cc = get_cc()
 except:  # noqa
     cc = None
