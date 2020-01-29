ef6985af9062cad6e73ac383c5d79c885ae17a42
Rob deCarvalho <robdmc@gmail.com>
2020-01-29 14:55:23 -0500

    added centered option to ecdf

--- easier/ecdf.py
+++ easier/ecdf.py
@@ -1,4 +1,4 @@
-def ecdf(x, N=100, inverse=False, as_percent=False):
+def ecdf(x, N=100, inverse=False, as_percent=False, centered=False):
     """
     Thin wrapper around statsmodels ecdf.
     Arguments:
@@ -15,6 +15,9 @@ def ecdf(x, N=100, inverse=False, as_percent=False):
     y_out = ECDF(x)(x_out)
     if inverse:
         y_out = 1 - y_out
+
+    if centered:
+        y_out = y_out - .5
 
     if as_percent:
         y_out = 100 * y_out
