a72f76a6c7f57e87810f7b3a66bd70eb0139ec38
Rob deCarvalho <robdmc@gmail.com>
2020-03-10 14:26:33 -0400

    updated twine publishing

--- publish_twine.py
+++ publish_twine.py
@@ -6,8 +6,8 @@ Here is the doc page for that.  I just haven't had the time to do it.
 https://pypi.org/project/twine/
 """
 
-# subprocess.call('pip install wheel'.split())
-# subprocess.call('pip install twine'.split())
+subprocess.call('pip install wheel'.split())
+subprocess.call('pip install twine'.split())
 subprocess.call('pwd'.split())
 subprocess.call('rm -rf ./build'.split())
 subprocess.call('rm -rf ./dist/'.split())
