eb72b122bf53abeeba76f200c5b0bfed382cd3b9
Generic User <generic@user.net>
2022-08-16 17:45:18 -0400

    wip

--- easier/__init__.py
+++ easier/__init__.py
@@ -1,75 +1,103 @@
 # flake8: noqa
-from .version import __version__
+# import sys
+from .api import *
 
-from .bernstein import Bernstein, BernsteinFitter
-from .clock import Clock, GlobalClock
-from .crypt import Crypt
-from .dataframe_tools import (
-    slugify,
-    pandas_utc_seconds_to_time,
-    pandas_time_to_utc_seconds,
-)
-from .distributions import DistFitter
-from .ecdf import ecdf
-from .fit import Fitter
-from .gsheet import GSheet
-from .hvtools import cc, hist, Animator, beta_plots
-from .item import Item
-from .iterify import iterify
-from .memory import mem_show, mem_get
-from .parallel import Parallel
-from .param_state import ParamState
-from .plotting import figure, get_cc
-from .postgres import PG
-from .print_catcher import PrintCatcher
-from .salesforce import SalesForceReport, Soql
-from .shaper import Shaper
-from .timer import Timer
-import easier.hvtools as hv  # Need this weird import to make hv symbol work 
-import easier.filtering as filtering
-from .nice_dates import nice_dates
-from .duck import Duck
-from .minimodel import MiniModel, MiniModelSqlite, MiniModelPG
+# from .api import EZR, Importable
+# xxx = EZR()
+# for k, v in EZR.__dict__.items():
+#     if isinstance(v, Importable):
+#         setattr(sys.modules[__name__], k, getattr(xxx, k))
+#         print(k, v, type(v), isinstance(v, Importable))
+# from .api.EZR import *
+# sys.modules['rob'] = EZR('rob')
+# from .version import __version__
+# from .api import _add_attributes_to_module
 
-# Alias for nice_dates
-date_formatter = nice_dates
+# def _add_attributes_to_module():
+#     import sys
+#     from .api import EZR, Importable
+#     ezr = EZR()
+#     for k, v in EZR.__dict__.items():
+#         if isinstance(v, Importable):
+#             setattr(sys.modules[__name__], k, getattr(ezr, k))
+#             print(k, v, type(v), isinstance(v, Importable))
 
-from .outlier_tools import (
-    kill_outliers_iqr,
-    kill_outliers_sigma_edit
-)
-from .utils import (
-    cached_container,
-    cached_dataframe,
-    cached_property,
-    mute_warnings,
-    pickle_cache_mixin,
-    pickle_cache_state,
-    pickle_cached_container,
-    print_error,
-    django_reconnect,
-    screen_width_full,
-    BlobMixin,
-    BlobAttr,
-    Scaler,
-    get_logger,
-)
+# _add_attributes_to_module()
 
-# alias for ease of remembering the name
-warnings_mute = mute_warnings
 
-# For backwards compatibility alias Fit to Fitter
-class Fit(Fitter):
-    def __init__(self, *args, **kwargs):
-        import warnings
-        warnings.warn('Fit is deprecated.  Use Fitter instead')
-        super().__init__(*args, **kwargs)
+# from .api import EZR
+# for k, v in EZR.__dict__.items():
+#     print(k, v, type(v))
 
-# loads a holoviews color cycler as cc defaulting to None if not available
-try:
-    cc = get_cc()
-except:  # noqa
-    cc = None
+# from .bernstein import Bernstein, BernsteinFitter
+# from .clock import Clock, GlobalClock
+# from .crypt import Crypt
+# from .dataframe_tools import (
+#     slugify,
+#     pandas_utc_seconds_to_time,
+#     pandas_time_to_utc_seconds,
+# )
+# from .distributions import DistFitter
+# from .ecdf import ecdf
+# from .fit import Fitter
+# from .gsheet import GSheet
+# from .hvtools import cc, hist, Animator, beta_plots
+# from .item import Item
+# from .iterify import iterify
+# from .memory import mem_show, mem_get
+# from .parallel import Parallel
+# from .param_state import ParamState
+# from .plotting import figure, get_cc
+# from .postgres import PG
+# from .print_catcher import PrintCatcher
+# from .salesforce import SalesForceReport, Soql
+# from .shaper import Shaper
+# from .timer import Timer
+# import easier.hvtools as hv  # Need this weird import to make hv symbol work 
+# import easier.filtering as filtering
+# from .nice_dates import nice_dates
+# from .duck import Duck
+# from .minimodel import MiniModel, MiniModelSqlite, MiniModelPG
+
+# # Alias for nice_dates
+# date_formatter = nice_dates
+
+# from .outlier_tools import (
+#     kill_outliers_iqr,
+#     kill_outliers_sigma_edit
+# )
+# from .utils import (
+#     cached_container,
+#     cached_dataframe,
+#     cached_property,
+#     mute_warnings,
+#     pickle_cache_mixin,
+#     pickle_cache_state,
+#     pickle_cached_container,
+#     print_error,
+#     django_reconnect,
+#     screen_width_full,
+#     BlobMixin,
+#     BlobAttr,
+#     Scaler,
+#     get_logger,
+# )
+
+# # alias for ease of remembering the name
+# warnings_mute = mute_warnings
+
+# # For backwards compatibility alias Fit to Fitter
+# class Fit(Fitter):
+#     def __init__(self, *args, **kwargs):
+#         import warnings
+#         warnings.warn('Fit is deprecated.  Use Fitter instead')
+#         super().__init__(*args, **kwargs)
+
+# # loads a holoviews color cycler as cc defaulting to None if not available
+# try:
+#     cc = get_cc()
+# except:  # noqa
+#     cc = None
 
 
 
