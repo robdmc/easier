31a4cefca0e7e9354428501db5cd141c0e23df29
Generic User <generic@user.net>
2022-11-06 16:24:15 -0500

    wip

---
+++ easier/duck_model.py
@@ -0,0 +1,134 @@
+import contextlib
+import os
+import re
+import easier as ezr
+
+
+@contextlib.contextmanager
+def duck_connection(duck_file_name):
+    import duckdb
+    con = duckdb.connect(duck_file_name, read_only=False)
+    try:
+        yield con
+    finally:
+        con.close()
+
+def run_query(connection, sql, fetch=True, **kwargs):
+    for key, value in kwargs.items():
+        connection.register(key, value)
+    connection.execute(sql)
+    if fetch:
+        return connection.fetchdf()
+
+
+class Table:
+    def __init__(self, duck_obj, table_name):
+        self.duck = duck_obj
+        self.table_name = table_name
+        self._exists_dict = {}
+
+    def __dir__(self):
+        return [
+            'df',
+            'head',
+            'insert',
+            'drop',
+        ]
+
+    @property
+    def exists(self):
+        if self._exists_dict.get('exists', None) is None:
+            df = self.query('PRAGMA show_tables')
+            self._exists_dict['exists'] = self.table_name in list(df.name)
+        return self._exists_dict['exists']
+
+    def _ensure_exists(self):
+        if not self.exists:
+            raise ValueError(f'Table {self.table_name} has not yet been populated in the database')
+
+    def query(self, sql, fetch=True, **kwargs):
+        return self.duck.query(sql, fetch=fetch, **kwargs)
+        # return run_query(self.connection, sql, fetch=fetch)
+
+    @property
+    def df(self):
+        self._ensure_exists()
+        df = self.query(f'SELECT * FROM {self.table_name}')
+        return df
+
+    def head(self, n=5):
+        self._ensure_exists()
+        return self.query(f'SELECT * FROM {self.table_name} LIMIT {n}')
+
+    def create(self, df):
+        self.drop()
+        # with duck_connection(self.duck.file_name) as connection:
+        #     connection.register('__df_in__', df)
+
+        self.query(
+            f'CREATE TABLE {self.table_name} AS SELECT * FROM __df_in__',
+            fetch=False,
+            __df_in__=df
+        )
+        self.exists_dict = {}
+
+    def insert(self, df):
+        self._ensure_exists()
+        self.query(
+            f"INSERT INTO {self.table_name} SELECT * FROM __df_in__", 
+            fetch=False,
+            __df_in__=df
+        )
+
+    def drop(self):
+        self.query(f'DROP TABLE IF EXISTS {self.table_name}', fetch=False)
+
+
+class Tables:
+    def __init__(self, duck_obj):
+        self.duck = duck_obj
+
+    def __dir__(self):
+        return ['create'] + self.duck.table_names
+
+    def create(self, name, df):
+        if name == 'create':
+            raise ValueError("You cannot name a table 'create'.  It is a reserved word.")
+
+        table = Table(self.duck, name)
+        table.create(df)
+
+    def __getattr__(self, name):
+        if name in self.duck.table_names:
+            table = Table(self.duck, name)
+            setattr(self, name, table)
+        else:
+            raise AttributeError(f'No attribute {name}')
+        return table
+
+    def __str__(self):
+        return repr(self.duck.table_names)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Duck:
+    def __init__(self, file_name='./duck.ddb', overwrite=False, read_only=False):
+        self.file_name = file_name
+        self._read_only = read_only
+
+        if overwrite and os.path.isfile(self.file_name):
+            os.unlink(self.file_name)
+
+        self.tables = Tables(self)
+
+    def query(self, sql, fetch=True, **kwargs):
+        with duck_connection(self.file_name) as conn:
+            return run_query(conn, sql, fetch=fetch, **kwargs)
+
+    @property
+    def table_names(self):
+        df = self.query('PRAGMA show_tables')
+        return sorted(df.name)
+
