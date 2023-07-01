21134026d302b3cb56ce1c15e0ff24d02419bc5c
Rob deCarvalho <robdmc@gmail.com>
2023-07-01 17:45:58 -0400

    fixed OrderedSchema and wrote tests

--- easier/ibis_tools.py
+++ easier/ibis_tools.py
@@ -1,7 +1,8 @@
 import contextlib
 import os
 import weakref
-from io import StringIO 
+from io import StringIO
+
 
 def _get_duck_connection(file_name, overwrite=False, read_only=False):
     """
@@ -82,19 +83,52 @@ def sql_to_frame(conn, sql):
     df = pd.DataFrame(res.fetchall(), columns=res._metadata.keys)
     return df
 
+
 def get_order_schema_class():
     import ibis
     import ibis.expr.datatypes as dtypes
+    from ibis.expr import schema as sch
     import pandas as pd
 
     class OrderedSchema(ibis.Schema):
         dt = dtypes
 
-        def ordered_apply_to(self, df):
+        def ordered_apply_to(self, df, strict=True):
+            """
+            Ensures the dataframe will have columns listed
+            in the same order as the schema definition.
+            Args:
+                strict:  If set to True (the default) ensures that the entire schema
+                        of the resulting dataframe has been properly typed.
+            """
+            # Make sure the input is a dataframe
             if not isinstance(df, pd.DataFrame):
                 raise ValueError("ordered_appy_to only defined for dataframes")
+
+            # Get the columns and datatypes for the schema
             cols, types = zip(*self.items())
+
+            # Limit the frame to have only the desired columns and
+            # copy it.  This avoids set with copy as well as ensures
+            # that the input frame is not mutated
             df = df[list(cols)].copy()
-            return self.apply_to(df)
+
+            # Apply the schema to the frame
+            self.apply_to(df)
+
+            # If strict comparison is requested
+            if strict and not df.empty:
+                # Infer the schema of the dataframe
+                actual = sch.infer(df)
+
+                # Created a new ibis schema that mirrors this one
+                expected = ibis.Schema(dict(self.items()))
+
+                # If the two schemas are not equal, raise an error
+                if actual != expected:
+                    s = f'\n\nExpected Schema:\n{expected}\n--------\nResulting Schema\n{actual}'
+                    raise TypeError(s)
+
+            return df
 
     return OrderedSchema
