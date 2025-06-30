0170638585a5c907101cb97b5fa65030a0c1abdc
Rob deCarvalho <robdmc@gmail.com>
2025-06-30 11:38:14 -0400

    wip

--- setup.py
+++
@@ -1,6 +0,0 @@
-# Deprecated: This package now uses pyproject.toml for configuration.
-# This file is kept for backward compatibility but pyproject.toml is preferred.
-
-from setuptools import setup
-
-setup()
