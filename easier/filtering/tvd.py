b4af7c42f091b99d10a5a801566b668b6ace488f
Rob deCarvalho <robdmc@gmail.com>
2025-07-12 18:29:08 -0400

    fixed filtering imports

--- easier/filtering/tvd.py
+++ easier/filtering/tvd.py
@@ -1,7 +1,3 @@
-from scipy import sparse
-from scipy.sparse import linalg
-import numpy as np
-
 def tvd(y, lamda, num_iter=20, return_cost=False):
     """
     Performs total variation denoising on a supplied numpy array
@@ -33,23 +29,26 @@ def tvd(y, lamda, num_iter=20, return_cost=False):
 
     It is also included in the references directory of this repository.
     """
+    from scipy import sparse
+    import numpy as np
+
     if not isinstance(y, np.ndarray):
-        raise ValueError('tvd can only be used on numpy arrays')
+        raise ValueError("tvd can only be used on numpy arrays")
     if not len(y.shape) == 1:
-        raise ValueError('tvd can only be used on flat arrays. Try y.flatten()')
+        raise ValueError("tvd can only be used on flat arrays. Try y.flatten()")
     if np.any(np.isnan(y)):
-        raise ValueError('tvd must not contain any nans')
+        raise ValueError("tvd must not contain any nans")
     N = len(y)
     y = np.expand_dims(y, -1)
     cost = np.zeros(num_iter)
-    I = sparse.identity(N, format='csr')
-    D = I[1:N, :] - I[:N - 1, :]
+    I = sparse.identity(N, format="csr")
+    D = I[1:N, :] - I[: N - 1, :]  # type: ignore
     DDT = D @ D.T
     x = y
     Dx = D @ x
     Dy = D @ y
     for k in range(num_iter):
-        F = sparse.diags(np.abs(Dx.flatten()) / lamda, format='csr') + DDT
+        F = sparse.diags(np.abs(Dx.flatten()) / lamda, format="csr") + DDT
         x = y - np.expand_dims(D.T @ sparse.linalg.spsolve(F, Dy), -1)
         Dx = D @ x
         cost[k] = 0.5 * np.sum((x - y) ** 2) + lamda * np.sum(np.abs(Dx))
